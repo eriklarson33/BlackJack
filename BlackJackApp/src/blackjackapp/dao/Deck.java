b5ee22d0074138ebdb2a8da4bb24a364cec2caa3
eriklarson33 <eriklarson33@msn.com>
2018-02-21 19:50:48 -0600

    Figuring out Deck Creation

--- BlackJackApp/src/blackjackapp/dao/Deck.java
+++ BlackJackApp/src/blackjackapp/dao/Deck.java
@@ -18,11 +18,41 @@ public class Deck {
     private Map<Integer, Card> gameDeck = new HashMap<>();
     
     private static final String[] suits = {"Diamonds", "Spades", "Clubs", "Hearts"};
-    private static final String[] rank = {"Ace", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    private static final String[] rank = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
     private static final int[] values = {1,2,3,4,5,6,7,8,9,10,11};
     
     
     private void createDeck() {
+        Card currentCard = new Card();
+        
+        for (int i = 0; i<=suits.length; i++) {
+            currentCard.setSuit(suits[i]);
+            
+            for (int j = 0; j<=rank.length; j++) {
+                currentCard.setRank(rank[j]);
+                
+                switch(rank[j]) {
+                    case "Ace":
+                        currentCard.setCardValue(1);
+                        break;
+                    case "2":
+                        currentCard.setCardValue(2);
+                        break;
+                    default:
+                        System.out.println("We have a problem assigning ranks.");
+                }
+            }
+        }
+        
+//        for (int i = 0; i <= 52; i++) {
+//            Card currentCard = new Card();
+//            currentCard.setCardId(i);
+//            for(int j= 0; j<suits.length; j++) {
+//                
+//            }
+//            
+//            
+//        }
         
         for (int cardValue : values) {
             System.out.println("hi.");
